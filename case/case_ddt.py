27cc6caf1e8858fee6ffac1db5ceff1b1b6d218f
ijokerm <fivemsbear@163.com>
2024-08-15 14:50:43 +0800

    python+unittest+requests+ddt 执行用例

--- case/case_ddt.py
+++ case/case_ddt.py
@@ -7,12 +7,14 @@
 @Date    ：2024/8/9 17:26
 unittest+ddt 实现用例的封装
 """
+import json
+import unittest
+
+import ddt
+
+import config
 from common.excel_handle import ExcelHandle
 from common.request_handle import RequestHandle
-import ddt
-import unittest
-import os
-import config
 
 
 @ddt.ddt
@@ -23,7 +25,34 @@ class Test_Api(unittest.TestCase):
 
     def setUp(self) -> None:
         self.req = RequestHandle()
-        login = self.req.visit('post',config.login_url,)
+        login = self.req.visit('post',config.login_url,json=config.userpwd,headers=config.header)
 
+        self.assertEqual(login.status_code,200)
     def tearDown(self) -> None:
+        # 关闭session
         self.req.close_session()
+    @ddt.data(*case_data)
+    def test_api(self,items):
+        # 请求接口
+        res = self.req.visit(method=items['method'],url=config.base_url+items['url'],
+                             json=json.loads(items['payload']),headers=json.loads(items['headers']))
+
+        try:
+            #   断言：预期结果与实际结果对比
+            self.assertEqual(res.status_code,items['expect_result'])
+            result = 'pass'
+        except AssertionError as e:
+            result = 'fail'
+            raise e
+        finally:
+            #    将响应的状态码写入testcase.xlsx第9列
+            Test_Api.excel.write_excel("../case/testcase.xlsx",'p1',items['case_id']+1,9,res.status_code)
+            #   如果断言成功，则在第10列写入测试结果‘pass’，否则写入‘fail’
+            Test_Api.excel.write_excel("../case/testcase.xlsx",'p1',items['case_id']+1,10,result)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
